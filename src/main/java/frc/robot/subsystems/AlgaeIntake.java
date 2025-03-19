b36baca4cabe36f08786e80f4d02d29aefb5f8cf
Liam Linster <liam.linster@gmail.com>
2025-03-18 18:42:36 -0600

    Algae arm for Falcon now JIT

--- src/main/java/frc/robot/subsystems/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntake.java
@@ -20,7 +20,7 @@ public class AlgaeIntake extends SubsystemBase {
     private static String tsSoFweakingStateVariable = "Calibrating";
     private static TstingAlgaeWristPID pid = new TstingAlgaeWristPID();
     private static double targetPos = 0;
-    private static double maxAlgaeArmRotations = -0.768;
+    private static double maxAlgaeArmRotations = 4;
 
     private static Timer intakeTimer = new Timer();
      // Update every 50ms
@@ -34,10 +34,11 @@ public class AlgaeIntake extends SubsystemBase {
     }
 
     public static void RunAlgaeIntake(double speed){
+        System.out.println("Rotation " + targetPos);
         //System.out.println(RobotContainer.algaeLimitSwitch.get());
             if(tsSoFweakingStateVariable.equals("Calibrating")) {
                 System.out.println(tsSoFweakingStateVariable);
-                algaeWrist.set(-0.05); // auto-calibrate speed; needs to go until it hits the switch
+                algaeWrist.set(-0.1); // auto-calibrate speed; needs to go until it hits the switch
                 if (RobotContainer.algaeLimitSwitch.get()) {
                     algaeWrist.set(0);
                     algaeWrist.setPosition(0);
@@ -64,21 +65,23 @@ public class AlgaeIntake extends SubsystemBase {
                     tsSoFweakingStateVariable = "Calibrating";
                 }
 
-                if (targetPos >=  maxAlgaeArmRotations  ) {
-                    algaeWrist.set(0);
-                    targetPos = maxAlgaeArmRotations;
+                if (targetPos >= maxAlgaeArmRotations  ) {
+                    targetPos -= speed/5;
                 }
                 if(targetPos < 0) {
-                    algaeWrist.set(0);
-                    targetPos = 0;
+                    targetPos += speed/5;
                 }
+                if(RobotContainer.algaeLimitSwitch.get() && algaeWrist.get() < 0) {
+                    algaeWrist.set(0);
+                }
+
 
                 // Intake/Outtake control
                 if(RobotContainer.leftTriggerAxis.getAsBoolean()) {
-                   top.set(-0.1);
+                   top.set(.5);
                   // bottom.set(ControlMode.PercentOutput, 1);
                 } else if (RobotContainer.rightTriggerAxis.getAsBoolean()) {
-                    top.set(0.1);
+                    top.set(-0.5);
                    //bottom.set(ControlMode.PercentOutput, -1);
                 } else {
                    top.set(0);
